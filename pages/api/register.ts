54521c651243fa26b5f90ce82859fb03293f8cc7
Omar <annexomar@gmail.com>
2023-03-30 23:16:22 +0300

    register endpoint

---
+++ pages/api/register.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcrypt";
+import prisma from "@/utils/client";
+
+interface User {
+  name: string;
+  password: string;
+  email: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<User>
+) {
+  if (req.method != "POST") {
+    return;
+  }
+  const { name, email, password } = req.body;
+  const encryptedPass = await bcrypt.hash(password, 10);
+  try {
+    const mailExist = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (mailExist) {
+      return res.status(200).send("Email already exists");
+    }
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: encryptedPass,
+        name,
+      },
+    });
+    return res.status(200).json(user);
+  } catch (error) {
+    throw new Error(error);
+  }
+}
